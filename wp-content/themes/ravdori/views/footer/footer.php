1bae43987608df067719c37d153506d0bcbe3187
Roy Hizkya <roy@htmline.com>
2018-12-25 09:59:48 +0000

    Changed logos in the footer

--- wp-content/themes/ravdori/views/footer/footer.php
+++ wp-content/themes/ravdori/views/footer/footer.php
@@ -4,19 +4,46 @@
 	
 		<div class="row">
 	
-			<div class="col-sm-4">
+			<div class="col-sm-4" style="max-width: 29%; padding-left:0;">
 				<?php
 					// footer menu
 					get_template_part('views/components/footer-menu');
 				?>
 			</div>
                
-               <div class="col-sm-5">
+               <div class="col-sm-5" style="padding:0;">
 				<div class="nadav-logo">
-                    <a href="http://www.bh.org.il/he/default.aspx" target="_blank">
-                        <img src="<?php echo IMAGES_DIR?>/general/footer/logo-footer.png" />
-                    </a>
-					<div class="text-center">&copy; כל הזכויות שמורות</div>
+                        
+						<div style="width:25%;float:right;">
+									<a href="https://www.seeingthevoices.com/skn/c82/%D7%A8%D7%95%D7%90%D7%99%D7%9D_%D7%90%D7%AA_%D7%94%D7%A7%D7%95%D7%9C%D7%95%D7%AA"  target="_blank">
+										<img src="<?php echo IMAGES_DIR?>/general/footer/voices-logo.png" alt=""/>
+									</a>
+						</div>
+
+						
+						<div style="width:25%;float:right;">
+									<a href="https://www.gov.il/he/Departments/General/kesher" target="_blank">
+										<img src="<?php echo IMAGES_DIR?>/general/footer/siv-logo.png" alt=""/>
+									</a>
+						</div>
+						
+						
+						<div style="width:25%;float:right;">
+									<a href="https://www.bh.org.il/he/%D7%97%D7%99%D7%A0%D7%95%D7%9A/%D7%94%D7%A7%D7%A9%D7%A8-%D7%94%D7%A8%D7%91-%D7%93%D7%95%D7%A8%D7%99/"  target="_blank">
+										<img src="<?php echo IMAGES_DIR?>/general/footer/ravdori-logo.png" alt=""/>
+									</a>
+						</div>
+
+
+						<div style="width:25%;float:right;">
+								<a href="https://www.bh.org.il/" target="_blank">
+										<img src="<?php echo IMAGES_DIR?>/general/footer/bh-logo.png" alt=""/>
+									</a>
+						</div>
+								
+		
+
+					<div class="text-center" style="float: right;width: 100%;">&copy; כל הזכויות שמורות</div>
 				</div>
 			</div>
                
