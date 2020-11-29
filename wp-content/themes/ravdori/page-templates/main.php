dbf1ea315059332caa4c0af76129b082234882ca
Roy Hizkya <roy@htmline.com>
2020-11-29 13:00:31 +0000

    Readded the qoutes to the homepage

--- wp-content/themes/ravdori/page-templates/main.php
+++ wp-content/themes/ravdori/page-templates/main.php
@@ -14,6 +14,7 @@ get_header();
                 get_template_part('views/main/layout', 'banner');
                 get_template_part('views/main/layout', 'news');
                 get_template_part('views/main/layout', 'rav-dori');
+				get_template_part('views/main/layout', 'quotes-dictionary');
 				get_template_part('views/main/layout', 'numbers-strip');
                 get_template_part('views/main/layout', 'useful-links');
             ?>
